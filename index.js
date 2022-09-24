b712f5b40a0a644d5c6fb50732dfd2d92cc69598
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2022-09-24 14:34:12 +0200

    change2

--- index.js
+++ index.js
@@ -7,36 +7,79 @@ canvas.height = 576;
 //creiamo un rettangolo dove si svolgerà il gioco
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-// creazione players
+//fornisce l'accelerazione di caduta dell'oggetto
+const gravity = 0.2
 
 // creazione oggetto sprite
 class Sprite{
-    constructor(position){
+    constructor({ position, velocity }){
         this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
     }
 
     draw(){
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    //metodo per muovere gli oggetti
+    update(){
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
     }
 }
 
 //creazione player
 const player = new Sprite({
-    x:0,
-    y:0
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+
+    }
 });
 
-player.draw();
 
 //creazione enemy
 const enemy = new Sprite({
-    x:400,
-    y:100
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+    
 });
 
-enemy.draw();
 
 console.log(player);
 
 //creazione animation loop
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
+}
+
+animate();
+
+// mive characters with eventListeners
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key);
+
+})
